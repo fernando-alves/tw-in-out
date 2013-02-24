4335318cece6d6b6a8970935b6854991f007e199
Alabê Duarte <alabeduarte@gmail.com>
2013-02-24 17:47:32 -0300

    add google_oauth2 authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,17 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable,
          :registerable,
-         :recoverable,
-         :rememberable,
-         :trackable,
-         :validatable,
-         :omniauthable
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password
 
-  def self.find_for_googleapps_oauth(access_token, signed_in_resource=nil)
-    data = access_token['info']
-    if user = User.where(:email => data['email']).first
-      return user
-    else #create a user with stub pwd
-      User.create!(:email => data['email'], :password => Devise.friendly_token[0,20])
+  def self.find_for_google_account(access_token, signed_in_resource=nil)
+    data = access_token.info
+    if user = User.where(:email => data["email"]).first
+      user
+    else
+      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
     end
   end
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session['devise.googleapps_data'] && session['devise.googleapps_data']['user_info']
-        user.email = data['email']
-      end
-    end
-  end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "#{n}@gmail.com" }
-    password "********"
   end
 end
