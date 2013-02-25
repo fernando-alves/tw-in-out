6546f83a3907c8a09d027f1087968fc3f0d411e2
Alabê Duarte and Fernando Alves <beach-recife+aduarte+fjunior@thoughtworks.com>
2013-02-25 18:00:13 -0300

    Creating punch scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130224184005) do
+ActiveRecord::Schema.define(:version => 20130225172506) do
+
+  create_table "punches", :force => true do |t|
+    t.datetime "time"
+    t.integer  "user_id"
+    t.string   "type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "punches", ["user_id"], :name => "index_punches_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",              :default => "", :null => false
---
+++ spec/factories/punches.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :punch do
+    time "2013-02-25 14:25:06"
+    user nil
+    type ""
+  end
+end
