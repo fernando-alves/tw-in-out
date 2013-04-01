30a7b7f9b19f284b22f13d0d634c7fb5d1bcb0a0
Alabê Duarte <alabeduarte@gmail.com>
2013-04-01 14:57:11 -0300

    Add regex to authorize only @thoughtworks.com email domain

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "authentication" do
+    context "only ThoughtWorkers can access" do
+      it { User.new(email: "xyz@thoughtworks.com").should be_valid }
+      it { User.new(email: "xyz@gmail.com").should_not be_valid }
+    end
+  end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "#{n}@gmail.com" }
+    sequence(:email) { |n| "#{n}@thoughtworks.com" }
   end
   factory :punch do
     time "2013-02-25 14:25:06"
