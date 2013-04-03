ba870abbf6070cf1cd2d76bbdd640ed865ab5da5
Alabê Duarte <alabeduarte@gmail.com>
2013-04-03 17:36:33 -0300

    Set the punch time values by the current time

--- app/assets/javascripts/currentTime.js
+++ app/assets/javascripts/currentTime.js
@@ -1,6 +1,6 @@
 CurrentTime = function(date) {
   this.year = date.getFullYear();
-  this.month = date.getMonth();
+  this.month = date.getMonth() + 1;
   this.date = date.getDate();
   this.hours = date.getHours();
   this.minutes = date.getMinutes();
@@ -9,11 +9,11 @@ CurrentTime = function(date) {
 CurrentTime.prototype.setCurrentTime = function() {
   $("#punch_time_1i, #punch_time_2i, #punch_time_3i, #punch_time_4i, #punch_time_5i").find("option:selected").removeAttr("selected");
 
-  $("#punch_time_1i option[value=" + this.year + "]").attr("selected", true);
-  $("#punch_time_2i option[value=" + this.month + "]").attr("selected", true);
-  $("#punch_time_3i option[value=" + this.date + "]").attr("selected", true);
-  $("#punch_time_4i option[value=" + this.format(this.hours) + "]").attr("selected", true);
-  $("#punch_time_5i option[value=" + this.format(this.minutes) + "]").attr("selected", true);
+  $("#punch_time_1i").val(this.year);
+  $("#punch_time_2i").val(this.month);
+  $("#punch_time_3i").val(this.date);
+  $("#punch_time_4i").val(this.format(this.hours));
+  $("#punch_time_5i").val(this.format(this.minutes));
 }
 
 CurrentTime.prototype.format = function(value) {
--- spec/javascripts/currentTimeSpec.js
+++ spec/javascripts/currentTimeSpec.js
@@ -2,7 +2,7 @@ describe("Current Time", function() {
   var currentTime;
   beforeEach(function() {
     loadFixtures("new_punch.html");
-    currentTime = new CurrentTime(new Date(2012, 1, 1, 9, 30));
+    currentTime = new CurrentTime(new Date(2012, 4, 3, 9, 30));
   });
   it("should load the template", function() {
     expect($('#punch')).toContain($("#punch_time_1i"));
@@ -13,22 +13,22 @@ describe("Current Time", function() {
   });
   it("should set the current year", function() {
     currentTime.setCurrentTime();
-    expect($("#punch_time_1i option[value=2012]")).toBeSelected();
+    expect($("#punch_time_1i").val()).toBe("2012");
   });
   it("should set the current month", function() {
     currentTime.setCurrentTime();
-    expect($("#punch_time_2i option[value=1]")).toBeSelected();
+    expect($("#punch_time_2i").val()).toBe("5");
   });
   it("should set the current date", function() {
     currentTime.setCurrentTime();
-    expect($("#punch_time_3i option[value=1]")).toBeSelected();
+    expect($("#punch_time_3i").val()).toBe("3");
   });
   it("should set the current hours", function() {
     currentTime.setCurrentTime();
-    expect($("#punch_time_4i option[value=09]")).toBeSelected();
+    expect($("#punch_time_4i").val()).toBe("09");
   });
   it("should set the current minutes", function() {
     currentTime.setCurrentTime();
-    expect($("#punch_time_5i option[value=30]")).toBeSelected();
+    expect($("#punch_time_5i").val()).toBe("30");
   });
 });
