384dfd96b3eb13959b2f259db6417c52ad4726c4
tworker <tworker@tworker-Latitude-E6510.(none)>
2013-04-04 16:33:44 -0300

    Added bootstrap table to views

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TwInOutWeb::Application.initialize!
+
+Date::DATE_FORMATS[:day] = "%d/%m/%Y"
+Time::DATE_FORMATS[:time] = "%H:%M"
