1f4eb62a4ac844abf05e5155e65486b1a0d5425f
Alabê Duarte <alabeduarte@gmail.com>
2013-04-18 10:39:12 -0300

    Improved test description

--- spec/presenters/workday_list_presenter_spec.rb
+++ spec/presenters/workday_list_presenter_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 describe WorkdayListPresenter do
-  context "given the number of worked hours" do
+  context "when has a list of  workdays" do
     workday_counter_presenter = WorkdayListPresenter.new(User.new, [Workday.new])
     workday = workday_counter_presenter.workdays.first
     it { workday.should be_a WorkdayPresenter }
     it { workday.worked_hours.should == "00:00" }
   end
+
 end
