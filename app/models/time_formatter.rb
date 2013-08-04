194c400c60c111b200235f15696dcd0c0ad14db5
Alabê Duarte <alabeduarte@gmail.com>
2013-08-04 20:54:02 -0300

    Logging TimeFormatter

--- app/models/time_formatter.rb
+++ app/models/time_formatter.rb
@@ -1,9 +1,13 @@
 class TimeFormatter
   def self.format(elapsed_time)
+    p "formating #{elapsed_time}"
+
     Time.at(elapsed_time).utc.strftime("%H:%M")
   end
 
   def self.format_total(elapsed_time)
+    p "formating #{elapsed_time}"
+
     hours = elapsed_time/3600
     (hours.to_f).round(1)
   end
