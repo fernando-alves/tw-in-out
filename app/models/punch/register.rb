43e2d0df1ca46d856c655cc35160e211e695d1b6
Alabê Duarte <aduarte@thoughtworks.com>
2015-04-06 11:56:58 -0300

    Refactoring Punch::Register specs

--- app/models/punch/register.rb
+++ app/models/punch/register.rb
@@ -1,11 +1,9 @@
 class Punch::Register
-
-  def self.punch(user, options)
-    punch = Punch.new options.merge(user_id: user.id)
+  def self.punch(user, attributes)
+    punch = Punch.new attributes.merge(user_id: user.id)
     if punch.time
       punch.workday = Workday.find_or_create_by_day(punch.time.to_date)
     end
     punch
   end
-
 end
