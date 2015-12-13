79877fb6e0059b10a21337e2e828883ef03d536b
Alabê Duarte <alabeduarte@gmail.com>
2015-12-13 14:13:31 -0300

    Add docker manual garbage collection

--- db/schema.rb
+++ db/schema.rb
@@ -11,37 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_130_429_192_718) do
+ActiveRecord::Schema.define(version: 20130429192718) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'punches', force: :cascade do |t|
-    t.datetime 'time'
-    t.integer 'user_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer 'workday_id'
+  create_table "punches", force: :cascade do |t|
+    t.datetime "time"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "workday_id"
   end
 
-  add_index 'punches', ['user_id'], name: 'index_punches_on_user_id', using: :btree
-  add_index 'punches', ['workday_id'], name: 'index_punches_on_workday_id', using: :btree
+  add_index "punches", ["user_id"], name: "index_punches_on_user_id", using: :btree
+  add_index "punches", ["workday_id"], name: "index_punches_on_workday_id", using: :btree
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email',              default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string 'name'
-    t.string 'role'
-    t.string 'labor_card'
-    t.date 'admission_date'
+  create_table "users", force: :cascade do |t|
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.string   "role"
+    t.string   "labor_card"
+    t.date     "admission_date"
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
-  create_table 'workdays', force: :cascade do |t|
-    t.date 'day'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "workdays", force: :cascade do |t|
+    t.date     "day"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
 end
--- script/start_docker.sh
+++ script/start_docker.sh
@@ -8,14 +8,9 @@ function log_action()  { _colored 3 "${1}"; } # use for warnings / attention
 function log_command() { _colored  "${1}"; } # use for debug messages
 
 readonly MACHINE_NAME="tw-in-out"
-readonly IS_OLDER_DOCKER=$(docker-machine -v | grep -c "0.3")
 readonly HAS_MACHINE=$(docker-machine ls | grep -c $MACHINE_NAME)
 readonly HAS_RUNNING_MACHINE=$(docker-machine ls | grep $MACHINE_NAME | grep -c "Running")
-
-if [[ $IS_OLDER_DOCKER -ne 0 ]]; then
-  log_error "Error: Your docker-machine version is older; Please update your docker-machine to > 0.3"
-  exit
-fi
+readonly HAS_GARBAGE=$(docker images -f "dangling=true" -q)
 
 if [ $HAS_MACHINE -eq 0 ]; then
   log_action "Creating machine ..."
@@ -36,3 +31,7 @@ fi
 log_action 'Done!'
 
 echo -e `docker-machine ls | grep ${MACHINE_NAME}`
+
+if [[ ! -z $HAS_GARBAGE ]]; then
+  docker rmi -f $HAS_GARBAGE
+fi
